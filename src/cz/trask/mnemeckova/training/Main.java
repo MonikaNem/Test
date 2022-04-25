2e6dadef8d8414ab8f8718c1d660c1cd3a7b51a9
Monika <dedvseved@seznam.cz>
2022-04-25 12:51:02 +0200

    Příspěvek jiného uživatele do master

--- src/cz/trask/mnemeckova/training/Main.java
+++ src/cz/trask/mnemeckova/training/Main.java
@@ -9,5 +9,6 @@ public class Main {
         public static void main(String[] args) {
             System.out.println("Čau!");
             System.out.println("Test změny přímo v masteru");
+            System.out.println("Příspěvek jiného uživatele do master");
         }
 }
