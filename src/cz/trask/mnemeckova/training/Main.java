df6dfb8248524c468f26be06723c97b385389244
Monika Nemečková <mnemeckova@thetrask.com>
2022-04-25 11:34:23 +0200

    Přidání větve monikaTest a provedení změny v této větvi

--- src/cz/trask/mnemeckova/training/Main.java
+++ src/cz/trask/mnemeckova/training/Main.java
@@ -9,5 +9,6 @@ public class Main {
         public static void main(String[] args) {
             System.out.println("Čau!");
             System.out.println("Test změny přímo v masteru");
+            System.out.println("Přidání větve monikaTest a provedení změny v této větvi");
         }
 }
