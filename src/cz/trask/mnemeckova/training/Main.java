162595c2de0eb07d2f229c4be8881e5bdea29375
Monika Nemečková <mnemeckova@thetrask.com>
2022-04-25 12:58:27 +0200

    První uživatel pokračuje v práci ve své větvi

--- src/cz/trask/mnemeckova/training/Main.java
+++ src/cz/trask/mnemeckova/training/Main.java
@@ -10,5 +10,6 @@ public class Main {
             System.out.println("Čau!");
             System.out.println("Test změny přímo v masteru");
             System.out.println("Přidání větve monikaTest a provedení změny v této větvi");
+            System.out.println("První uživatel pokračuje v práci ve své větvi");
         }
 }
